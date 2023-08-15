9a580834de021c3baf2aba234fc611f39725df75
ZipZapZeps <richards.email@web.de>
2023-08-15 10:58:15 +0200

    Added support for RBF Neurons

--- micrograd/nn.py
+++ micrograd/nn.py
@@ -3,6 +3,9 @@ from micrograd.engine import Value
 
 class Module:
 
+    def __init__(self) -> None:
+        self.active = True
+
     def zero_grad(self):
         for p in self.parameters():
             p.grad = 0
@@ -16,27 +19,51 @@ class Module:
 
 class Neuron(Module):
 
-    def __init__(self, nin, name, nonlin=True):
+    def __init__(self, nin, name, activation_func=Value.noop):
+        super().__init__()
         self.w = [Value(random.uniform(-1,1),_op= f"{name}w{i}") for i in range(nin)]
-        self.b = Value(random.uniform(-1,1), _op = f"{name}b")
-        self.nonlin = nonlin
-        self.name = name
+        self.b = Value(0., _op = f"{name}b")
+        self.activation_func = activation_func
 
     def __call__(self, x):
-        wx = sum(wi*xi for wi,xi in zip(self.w, x))
-        act = wx + self.b
-        return act.softplus() if self.nonlin else act
+        if self.active:
+            wx = sum(wi*xi for wi,xi in zip(self.w, x))
+            act = wx + self.b
+            return self.activation_func(act)
+        
+        return Value(0.)
 
     def parameters(self):
         return self.w + [self.b]
 
     def __repr__(self):
-        return f"{'ReLu' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
+        return f"{self.activation_func.__name__.title()}Neuron({len(self.w)})"
 
+class RBFNeuron(Module):
+    def __init__(self, nin, name, activation_func=Value.noop):
+        super().__init__()
+        self.w = [Value(random.uniform(-1,1),_op= f"{name}w{i}") for i in range(nin)]
+        self.b = Value(random.uniform(1.,2.), _op = f"{name}b")
+        self.activation_func = activation_func
+
+    def __call__(self, x):
+        if self.active:
+            wx = sum((wi+xi) ** 2. for wi,xi in zip(self.w, x)) ** 0.5
+            act = wx * -1. + self.b
+            return self.activation_func(act)
+        return Value(0.)
+
+    def parameters(self):
+        return self.w + [self.b]
+
+    def __repr__(self):
+        return f"{self.activation_func.__name__.title()}RBFNeuron({len(self.w)})"
+    
 class Layer(Module):
 
-    def __init__(self, nin, nout, name, **kwargs):
-        self.neurons = [Neuron(nin, f"{name}n{i}", **kwargs) for i in range(nout)]
+    def __init__(self, name:str, neurons):
+        super().__init__()
+        self.neurons = neurons
         self.name = name
 
     def __call__(self, x):
@@ -53,7 +80,24 @@ class MLP(Module):
 
     def __init__(self, nin, nouts):
         sz = [nin] + nouts
-        self.layers = [Layer(sz[i], sz[i+1], f"l{i}", nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
+        layers = []
+
+        def generate_neurons(nin:int, nout:int, name:str, last_layer:bool, **kwargs):
+            for i in range(nout):
+                name = f".{name}n{i}"
+                if last_layer:
+                    yield Neuron(nin, name)
+                elif i % 2:
+                    yield Neuron(nin, name, **kwargs)
+                else:
+                    yield RBFNeuron(nin, name, **kwargs)
+
+        for i in range(len(nouts)):
+            name = f"l{i}"
+            layer = Layer(name, list(generate_neurons(sz[i],sz[i+1],name,i+1==len(nouts),activation_func=Value.relu)))
+            layers.append(layer)
+
+        self.layers = layers
 
     def __call__(self, x):
         for layer in self.layers:
